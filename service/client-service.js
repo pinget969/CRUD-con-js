fc5151e60044cf019dc5329b96056a7641aef321
Nelson <njpinget@gmail.com>
2022-08-11 11:22:41 -0300

    subiendo archivo clase 1

---
+++ service/client-service.js
@@ -0,0 +1,47 @@
+const crearNuevaLinea = (nombre, email) =>{
+    const linea = document.createElement("tr")
+    const contenido = ` 
+            <td class="td" data-td>
+            ${nombre}
+            </td>
+            <td>${email}</td>
+            <td>
+              <ul class="table__button-control">
+                <li>
+                  <a
+                    href="../screens/editar_cliente.html"
+                    class="simple-button simple-button--edit"
+                    >Editar</a
+                  >
+                </li>
+                <p class = "texto_p"> la wea
+                </p>
+                <li>
+                  <button
+                    class="simple-button simple-button--delete"
+                    type="button"
+                  >
+                    Eliminar
+                  </button>
+                </li>
+              </ul>
+            </td>
+          `;
+    linea.innerHTML = contenido;
+    return linea;
+};
+const table = document.querySelector("[data-table]");
+//Llamada al Back
+//Fetch API
+const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+
+
+listaClientes()
+  .then((data)=>{
+    data.forEach(perfil =>{
+      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+      table.appendChild(nuevaLinea);
+  });
+  })
+  .catch((error) => alert(("Ocurrió un error")));
+
